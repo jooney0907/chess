794c4eb8f5a8b6578287164ba0ee0c22f83b416f
Sangjoon Ryu <sangjoonryu@gmail.com>
2026-09-18 18:32:03 -0600

    implemented bishop move to empty square, enemy on square, ally on square

--- shared/src/main/java/chess/Rule.java
+++ shared/src/main/java/chess/Rule.java
@@ -15,6 +15,34 @@ public class Rule {
     }
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition position){
         Collection<ChessMove> moves = new HashSet<>();
+        ChessPiece movingPiece = board.getPiece(position);
+        for(int[] direction: directions){
+            int rowChange = direction[0];
+            int columnChange = direction[1];
+            int newRow = position.getRow() + rowChange;
+            int newColumn = position.getColumn() + columnChange;
+            while (inBoundary(newRow, newColumn)){
+                ChessPosition newPosition = new ChessPosition(newRow, newColumn);
+                ChessPiece pieceLocation = board.getPiece(newPosition);
+                if(pieceLocation == null){
+                    ChessMove move = new ChessMove(position,newPosition, null);
+                    moves.add(move);
+                } else {
+                    if (pieceLocation.getTeamColor() == movingPiece.getTeamColor()) {
+                        break;
+                    }
+                    else{
+                    ChessMove move = new ChessMove(position, newPosition,null);
+                    break;
+                    }
+                    }
+                {
+                }
+                newRow += rowChange;
+                newColumn += columnChange;
+            }
+        }
+
         return moves;
     }
 }
